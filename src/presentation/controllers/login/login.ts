81fbaa2fe025b8793293fd71607f369c95398d16
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2022-08-16 17:53:07 +0200

    refactor: remove promise calls

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -17,13 +17,13 @@ export class LoginController implements Controller {
       const fields = ['email', 'password']
       for (const field of fields) {
         if (!httpRequest.body[field]) {
-          return new Promise((resolve) => resolve(badRequest(new MissingParamError(field))))
+          return badRequest(new MissingParamError(field))
         }
       }
       const { email, password } = httpRequest.body
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
-        return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))))
+        return badRequest(new InvalidParamError('email'))
       }
       await this.authentication.auth(email, password)
       return ok({
