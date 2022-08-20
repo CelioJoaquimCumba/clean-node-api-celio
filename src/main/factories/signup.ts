bd6d08409acffe8a26d49069b2cfd81e4c0377b4
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2022-08-20 17:12:38 +0200

    refactor: fix typo from deorators to decorators

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,7 +4,7 @@ import { DbAddAccount } from '../../data/usecases/add-acount/db-add-account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { Controller } from '../../presentation/protocols'
-import { LogControllerDecorator } from './deorators/log'
+import { LogControllerDecorator } from './decorators/log'
 import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 
 export const makeSignUpController = (): Controller => {
