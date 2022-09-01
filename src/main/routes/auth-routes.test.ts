f27bf478a89d0bad3833dc4e63e8d1257e418f45
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2022-09-01 17:29:44 +0200

    refactor: rename signup routes with auth routes

--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Auth Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
   })
@@ -15,16 +15,17 @@ describe('SignUp Routes', () => {
   //   const accountCollection = await MongoHelper.getCollection('accounts')
   //   await accountCollection.deleteMany({})
   // })
-
-  test('Should return an account on sucess', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Celio',
-        email: 'celio@example.com',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200)
+  describe('POST /SignUp Routes', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Celio',
+          email: 'celio@example.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })
